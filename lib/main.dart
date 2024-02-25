d0662976dc065350d53890f7bca02c35d3ebd61b
paulamolinalinan <paulaml2003@gmail.com>
2024-02-25 19:42:43 +0100

    gestion

--- lib/main.dart
+++ lib/main.dart
@@ -84,13 +84,6 @@ class _StartPageState extends State<StartPage> {
               ),
             ),
             const SizedBox(height: 20.0),
-            ElevatedButton(
-              child: Text('Peluqueros'),
-              onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(builder: (_) => TableComplexExample()),
-              ),
-            ),
           ],
         ),
       ),
