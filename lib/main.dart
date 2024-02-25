8357c1269a67477fbefbaba416a060d8bfce7986
Laesx <losguardianes15@gmail.com>
2024-02-25 20:10:59 +0100

    Añadido Parte de Paula y Cambiado Git Ignore

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,3 @@
-// Importa todas las pantallas que quieres exportar
-// ignore: depend_on_referenced_packages
-import 'package:flutter_peluqueria/screens/home_screen.dart';
-import 'package:flutter_peluqueria/screens/mi_app_bar.dart';
-import 'package:flutter_peluqueria/screens/mi_menu_desplegable.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'screens/screens.dart';
