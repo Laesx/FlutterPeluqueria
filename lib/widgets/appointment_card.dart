92ab3a0568f547210a59f2895b071f0333a6f7dc
carlosbonillafdez02 <carlosbonillafdez02@gmail.com>
2024-03-02 17:29:50 +0100

    cambios card reserva y reservas screen

--- lib/widgets/appointment_card.dart
+++ lib/widgets/appointment_card.dart
@@ -19,99 +19,102 @@ class AppointmentCard extends StatelessWidget {
 
 
   double calculateTotal() {
-    double total = 0.0;
-    for (var service in services) {
-      if (service['price'] != null) {
-        total += double.parse(service['price']!);
-      }
+  double total = 0.0;
+  for (var service in services) {
+    if (service['price'] != null && double.tryParse(service['price']!) != null) {
+      total += double.parse(service['price']!);
     }
-    return total;
   }
+  return total;
+}
+
 
   @override
   Widget build(BuildContext context) {
   final double total = calculateTotal();
 
-    return Card(
-      margin: const EdgeInsets.all(16.0),
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              'Nombre peluquería',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-            ),
-            const SizedBox(height: 8.0),
-            const Text(
-              'Dirección Peluquería',
-              style: TextStyle(color: Colors.grey),
-            ),
-            Text(
-              name,
-              style: const TextStyle(color: Colors.grey),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(time, style: const TextStyle(color: Colors.grey)),
-                Text(date, style: const TextStyle(color: Colors.grey)),
-              ],
-            ),
-            const SizedBox(height: 16.0),
-            const Divider(),
-            const SizedBox(height: 16.0),
-            DataTable(
-              columns: const [
-                DataColumn(label: Text('SERVICIO')),
-                DataColumn(label: Text('PRECIO')),
-              ],
-              rows: services
-                  .map(
-                    (service) => DataRow(
-                      cells: [
-                        DataCell(Text(service['name'] ?? '', style: const TextStyle(color: Colors.grey))),
-                        DataCell(Text(service['price'] ?? '', style: const TextStyle(color: Colors.grey))),
-                      ],
-                    ),
-                  )
-                  .toList(),
-            ),
-            const SizedBox(height: 16.0),
-            const Divider(),
-            const SizedBox(height: 16.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Total',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                Text(total.toString()),
-              ],
-            ),
-            const SizedBox(height: 16.0),
-            const Divider(),
-            const SizedBox(height: 16.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Método de pago: ',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                Text(paymentMethod),
-              ],
-            ),
-            const SizedBox(height: 16.0),
-            const Text('Código', style: TextStyle(color: Colors.grey)),
-            const SizedBox(height: 16.0),
-            if(verified)
-            const Text('PAGO VERIFICADO', style: TextStyle(color: Colors.green)),
-            if(!verified)
-            const Text('PAGO NO VERIFICADO', style: TextStyle(color: Colors.red))
-          ],
+    return SingleChildScrollView(
+      child: Card(
+        margin: const EdgeInsets.all(16.0),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Nombre peluquería',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+              const SizedBox(height: 8.0),
+              const Text(
+                'Dirección Peluquería',
+                style: TextStyle(color: Colors.grey),
+              ),
+              Text(
+                name,
+                style: const TextStyle(color: Colors.grey),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(time, style: const TextStyle(color: Colors.grey)),
+                  Text(date, style: const TextStyle(color: Colors.grey)),
+                ],
+              ),
+              const SizedBox(height: 16.0),
+              const Divider(),
+              const SizedBox(height: 16.0),
+              DataTable(
+                columns: const [
+                  DataColumn(label: Text('SERVICIO')),
+                  DataColumn(label: Text('PRECIO')),
+                ],
+                rows: services
+                    .map(
+                      (service) => DataRow(
+                        cells: [
+                          DataCell(Text(service['name'] ?? '', style: const TextStyle(color: Colors.grey))),
+                          DataCell(Text(service['price'] ?? '', style: const TextStyle(color: Colors.grey))),
+                        ],
+                      ),
+                    )
+                    .toList(),
+              ),
+              const SizedBox(height: 16.0),
+              const Divider(),
+              const SizedBox(height: 16.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Total',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(total.toString()),
+                ],
+              ),
+              const SizedBox(height: 16.0),
+              const Divider(),
+              const SizedBox(height: 16.0),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Método de pago: ',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(paymentMethod),
+                ],
+              ),
+              const SizedBox(height: 16.0),
+              const Text('Código', style: TextStyle(color: Colors.grey)),
+              const SizedBox(height: 16.0),
+              if(verified)
+              const Text('PAGO VERIFICADO', style: TextStyle(color: Colors.green)),
+              if(!verified)
+              const Text('PAGO NO VERIFICADO', style: TextStyle(color: Colors.red))
+            ],
+          ),
         ),
       ),
     );
--- lib/widgets/calendar_reservations_test.dart
+++ lib/widgets/calendar_reservations_test.dart
@@ -3,6 +3,7 @@ import 'package:flutter_peluqueria/models/models.dart';
 import 'package:flutter_peluqueria/widgets/widgets.dart';
 import 'package:table_calendar/table_calendar.dart';
 import '../utils.dart';
+import 'package:flutter/services.dart';
 
 class CalendarReservationsTest extends StatefulWidget {
   const CalendarReservationsTest({super.key});
@@ -16,17 +17,14 @@ class _CalendarReservationsTestState extends State<CalendarReservationsTest> {
   late final ValueNotifier<Horario> _selectedSchedule;
   final CalendarFormat _calendarFormat = CalendarFormat.week;
   final RangeSelectionMode _rangeSelectionMode = RangeSelectionMode.disabled;
-  // Este de abajo se puede cambiar por el de arriba para que se pueda seleccionar un rango de fechas
-  // Manteniendo pulsado el de abajo se puede seleccionar un rango de fechas
-  //RangeSelectionMode _rangeSelectionMode2 = RangeSelectionMode.toggledOff;
-
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
+
+  final ReservasServices _reservasServices = ReservasServices();
 
   @override
   void initState() {
     super.initState();
-
     _selectedDay = _focusedDay;
     _selectedSchedule = ValueNotifier(_getScheduleForDay(_selectedDay!));
   }
@@ -44,7 +42,6 @@ class _CalendarReservationsTestState extends State<CalendarReservationsTest> {
       peluquero: 'Peluquero 1',
       horario: [start, end],
     );
-
     return horario;
   }
 
@@ -54,9 +51,37 @@ class _CalendarReservationsTestState extends State<CalendarReservationsTest> {
         _selectedDay = selectedDay;
         _focusedDay = focusedDay;
       });
-
       _selectedSchedule.value = _getScheduleForDay(selectedDay);
     }
+  }
+
+  void _mostrarTarjeta(BuildContext context, Reserva reserva) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          padding: EdgeInsets.all(16.0),
+          child: AppointmentCard(
+            name: reserva.peluquero,
+            time: reserva.fecha[0]
+                .toString(), // Mostrar la primera fecha de la lista de fechas
+            date: reserva.fecha[0]
+                .toString(), // Mostrar la primera fecha de la lista de fechas
+            services: reserva.servicios.keys.map((servicioId) {
+              // Aquí podrías obtener los detalles del servicio si es necesario
+              return {'name': servicioId, 'price': ''};
+            }).toList(),
+            paymentMethod: reserva.pago,
+            verified: true, // Cambiar según sea necesario
+          ),
+        );
+      },
+    );
+  }
+
+  Future<void> _obtenerReserva(String usuario) async {
+    Reserva reserva = await _reservasServices.getReservaById(usuario);
+    _mostrarTarjeta(context, reserva);
   }
 
   @override
@@ -100,7 +125,11 @@ class _CalendarReservationsTestState extends State<CalendarReservationsTest> {
                       borderRadius: BorderRadius.circular(12.0),
                     ),
                     child: ListTile(
-                      onTap: () => print('${horario.peluquero}'),
+                      onTap: () {
+                        print('${horario.peluquero}');
+                        //String usuario = obtenerUsuario(); // Debes implementar esta función para obtener el ID del usuario
+                        _obtenerReserva(usuario);
+                      },
                       title: Text('${horario.toMap()["horario"]}'),
                     ),
                   );
