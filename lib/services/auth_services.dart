ecd32948c843a2432047aaa9c99a2f87ebcfdc7a
Laesx <losguardianes15@gmail.com>
2024-03-03 21:14:21 +0100

    2 Cosas hechas pero no funcionan

--- lib/services/auth_services.dart
+++ lib/services/auth_services.dart
@@ -1,13 +1,14 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 
 class AuthService extends ChangeNotifier {
   final String _baseUrl = 'identitytoolkit.googleapis.com';
   final String _firebaseToken = 'AIzaSyDKMEVgfMzZqfgxidnCh0Sn0d5C67Y-dfI';
 
-  late final String idToken;
+  final storage = FlutterSecureStorage();
 
   //Si devolmenos algo, es un error, si no, toco correcto
   Future<String?> createUser(String email, String password) async {
@@ -44,11 +45,15 @@ class AuthService extends ChangeNotifier {
 
     final Map<String, dynamic> decodedResp = json.decode(resp.body);
 
-    if (decodedResp.containsKey('idToken')) {
-      idToken = decodedResp['idToken'];
+    print(resp.toString());
 
-      return decodedResp['idToken'];
-      //return null;
+    if (decodedResp.containsKey('idToken')) {
+      final idToken = decodedResp['idToken'];
+      // Store the ID token
+      await storage.write(key: 'idToken', value: idToken);
+
+      //return decodedResp['idToken'];
+      return null;
     } else {
       return decodedResp['error']['message'];
     }
